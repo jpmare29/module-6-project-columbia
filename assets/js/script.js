cb586f2f2acc3b03d598069b4899efd03adb9395
jpmare29 <jpmmare@yahoo.com>
2022-04-06 14:15:26 -0400

    initial form, recipe api test

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,11 @@ var url = "www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita";
   value="Generate"
 /> */
 }
+
+const recipeUrl = 'https://api.edamam.com/api/recipes/v2?type=public&q=dairy%20free&app_id=1dde8709&app_key=a8d10560bc3d45c586fa2de0978a729b';
+
+const test = fetch(recipeUrl).then(response => {
+   return response.json();
+}).then(data => {
+    console.log(data);
+})
