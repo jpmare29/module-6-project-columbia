ac0c1b6c98791b3b9112a061e28cb208ef661bdc
jpmare29 <jpmmare@yahoo.com>
2022-04-07 17:47:57 -0400

    still testing api

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,10 +13,28 @@ var url = "www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita";
 /> */
 }
 
-const recipeUrl = 'https://api.edamam.com/api/recipes/v2?type=public&q=dairy%20free&app_id=1dde8709&app_key=a8d10560bc3d45c586fa2de0978a729b';
+//const recipeUrl = 'https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=1dde8709&app_key=a8d10560bc3d45c586fa2de0978a729b';
 
-const test = fetch(recipeUrl).then(response => {
-   return response.json();
-}).then(data => {
+const userForm = document.getElementById('user-form');
+const searchedIngredient = document.getElementById('recipe');
+
+
+const formSubmitHandler = event => {
+  event.preventDefault();
+  let mainIngredient = searchedIngredient.value;
+  let searchUrl = `https://api.edamam.com/api/recipes/v2?type=public&q=${mainIngredient}&app_id=1dde8709&app_key=a8d10560bc3d45c586fa2de0978a729b`
+  fetch(searchUrl).then(response => {
+    return response.json();
+  }).then(data => {
+    for (let i = 0; i < 8; i++) {
+      let ingredientArray = data.hits[i].recipe.ingredientLines;
+      ingredientArray.forEach(element => {
+        console.log(element);
+      })
+      console.log(data.hits[i].recipe.ingredientLines);
+    }
     console.log(data);
-})
+  })
+}
+
+userForm.addEventListener('submit', formSubmitHandler);
