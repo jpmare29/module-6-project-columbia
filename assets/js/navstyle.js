7af0eefda9dbbc987a7f0c4d20e75eeefb294bb4
Mahamadou Kabba <mkabba34@gmail.com>
2022-04-12 19:14:10 -0400

    work on navstyle.js

--- assets/js/navstyle.js
+++ assets/js/navstyle.js
@@ -1,22 +1,26 @@
 // DOm
-var aboutBox = document.querySelector("#about-box");
-var aboutNav = document.querySelector("#about-nav");
+let aboutBox = document.querySelector("#about-box");
+let aboutNav = document.querySelector("#about-nav");
 // ---------
-var recipeBox = document.querySelector("#card-container");
-var recipeNav = document.querySelector("#recipe-nav");
+let recipeBox = document.querySelector("#card-container");
+let recipeNav = document.querySelector("#recipe-nav");
 // ------
-var searchNav = document.querySelector("#search-nav");
+let searchNav = document.querySelector("#search-nav");
+let searchInput = document.querySelector("#recipe-box");
 
 // this displays about paragraph (please change this if you need to)
 aboutNav.addEventListener("click", function () {
   aboutBox.style.display = "block";
+  searchInput.style.display = "none";
   recipeBox.style.display = "none";
 });
 recipeNav.addEventListener("click", function () {
   aboutBox.style.display = "none";
+  searchInput.style.display = "none";
   recipeBox.style.display = "block";
 });
 searchNav.addEventListener("click", function () {
   aboutBox.style.display = "none";
   recipeBox.style.display = "none";
+  searchInput.style.display = "block";
 });
